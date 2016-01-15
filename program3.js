90f3b33ffd38253f33a6bb684f7ef2b660c94e06
Mohonish Chakraborty <mohonish.c@gmail.com>
2016-01-15 12:40:19 +0530

    done program 4

--- program3.js
+++ program3.js
@@ -3,6 +3,6 @@ var fs = require('fs')
 var pathToFile = process.argv[2] //passed as first argument.
 var fileBuf = fs.readFileSync(pathToFile) //read file
 var fileStr = fileBuf.toString() //filebuffer to string
-var resultArr = fileStr.split("\n") //split string to array using delimiter.
+var resultArr = fileStr.split('\n') //split string to array using delimiter.
 
-console.log(resultArr.length + 1)
+console.log(resultArr.length - 1)
